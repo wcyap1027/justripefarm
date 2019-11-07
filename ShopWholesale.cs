a01e351b4692397a85943717e892efe38173a72a
DESKTOP-UV6K0KC\yongj <yongjia.0626@gmail.com>
2019-11-07 10:11:07 +0800

    add ShopWholesale class

---
+++ ShopWholesale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Multi_Form_and_Panel
+{
+	public class ShopWholesale
+	{
+        private int quantity;
+        private double total;
+        private string itemName;
+        private int stockAvailable;
+
+        public int Quantity { get => quantity; set => quantity = value; }
+        public double Total { get => total; set => total = value; }
+        public string ItemName { get => itemName; set => itemName = value; }
+        public int StockAvailable { get => stockAvailable; set => stockAvailable = value; }
+    }
+}
